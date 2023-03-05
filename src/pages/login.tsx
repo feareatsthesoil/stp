ac4694297ef6326775ec5953fc5abe6879e49424
Jonathan <jonathan@Jonathans-MacBook-Pro.local>
2023-03-05 14:05:20 +0200

    Added welcome page and changed link colors

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -17,7 +17,7 @@ const Welcome = () => {
           <button>Connect with Wallet</button>
           <div className={index.label}><label htmlFor="textarea"> Email</label><hr/><textarea id="textarea" name="email" className={index.textarea} placeholder="info@stp.world"/></div>
           <div className={index.bottomButton}><button id="email">Continue with email</button></div>
-          <Link href="/contactinfo">Forgot Password?</Link>
+          <Link href="/welcome">Forgot Password?</Link>
         </div>
         <Footer />
       </div>
