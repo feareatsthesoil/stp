624846e89d4da381d6c00c188506a0c860f8f436
feareatsthesoil <105951475+feareatsthesoil@users.noreply.github.com>
2023-03-05 15:53:38 -0500

    Update discussion.tsx

--- src/pages/discussion.tsx
+++ src/pages/discussion.tsx
@@ -24,16 +24,16 @@ const Discussion = () => {
                     </div>
                     <h1>Urbit</h1>
                     <p>
-                        The STP community offers a variety of programming and events, including a weekly
-                        in person meeting led by artists in the community. Members can also enjoy weekly
-                        essays and other writing on STP&apos;s blog and newsletter, which also feature updates
-                        from the STP community.
+                        Urbit is a new OS and peer-to-peer network that is designed to be simple, 
+                        built to last forever, and 100% owned by its users. Urbit&apos;s Landscape, a collection 
+                        of native apps for communicating and collaborating on the network, presents the possibility 
+                        of a new way forward.
                     </p>
+                    Serving the People has partnered with the Tlon Corporation to offer free hosting 
+                    (usually $20/month) to members of Serving the People. Join us and a cohort of 
+                    communities making the move to Urbit's Landscape.
                     <p>
-                        The STP community offers a variety of programming and events, including a weekly
-                        in person meeting led by artists in the community. Members can also enjoy weekly
-                        essays and other writing on STP&apos;s blog and newsletter, which also feature updates
-                        from the STP community.
+                       
                     </p>
                     <div className={index.buttonBody}>
                         <button className={index.button}>
