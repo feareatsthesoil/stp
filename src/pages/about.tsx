abba28e5aab27219493481f5ff99a67369b128ce
Jonathan <jonathan@jonathans-mbp.attlocal.net>
2023-03-25 12:57:21 -0700

    Add about and tsconfig

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,39 +9,39 @@ const About = () => {
     <>
       <div className="body">
         <Header />
-        <Nav/>
-    <div className="subBody">
-      
-        <div className={index.box}>
-          <h1>About</h1>
-          <p>
-            Serving the People is a 501(c)(3) non-profit organization that assists artists
-            and creators in making meaningful connections both online and in person.
-            Established in 2017, STP has launched a number of initiatives and developed a
-            platform for connecting creators with audiences, as well as finding opportunities
-            for collaboration and support.
-          </p>
-          <p>
-            The STP community offers a variety of programming and events, including a weekly
-            in person meeting led by artists in the community. Members can also enjoy weekly
-            essays and other writing on STP&apos;s blog and newsletter, which also feature updates
-            from the STP community.
-          </p>
-          <p>
-            <strong>Subscribe to our weekly newsletter for happenings and events</strong>
-          </p>
-          <div className={index.input}>
-            <div className={index.text}>
-              <textarea 
-                name="email" 
-                placeholder="info@stp.world" 
-              />
-            </div>
-            <div className={index.buttonBody}>
-              <button className={index.button}>Subscribe</button>
+        <Nav />
+        <div className="subBody">
+
+          <div className={index.box}>
+            <h1>About</h1>
+            <p>
+              Serving the People is a 501(c)(3) non-profit organization that assists artists
+              and creators in making meaningful connections both online and in person.
+              Established in 2017, STP has launched a number of initiatives and developed a
+              platform for connecting creators with audiences, as well as finding opportunities
+              for collaboration and support.
+            </p>
+            <p>
+              The STP community offers a variety of programming and events, including a weekly
+              in person meeting led by artists in the community. Members can also enjoy weekly
+              essays and other writing on STP&apos;s blog and newsletter, which also feature updates
+              from the STP community.
+            </p>
+            <p>
+              <strong>Subscribe to our weekly newsletter for happenings and events</strong>
+            </p>
+            <div className={index.input}>
+              <div className={index.text}>
+                <textarea
+                  name="email"
+                  placeholder="info@stp.world"
+                />
+              </div>
+              <div className={index.buttonBody}>
+                <button className={index.button}>Subscribe</button>
+              </div>
             </div>
           </div>
-        </div>
         </div>
         <Footer />
       </div>
