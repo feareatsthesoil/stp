82de4bb036d812f3156614915faaee0dcc7686b8
Jonathan <jonathan@jonathans-mbp.attlocal.net>
2023-04-01 14:16:16 -0700

    Updated cal profile

--- src/pages/calendar/submit.tsx
+++ src/pages/calendar/submit.tsx
@@ -59,7 +59,7 @@ export default function CalendarSubmit() {
 
             </div>
 
-            <CalendarForm />
+            <CalendarForm profile={false}/>
           </div>
         </div>
         <Footer />
