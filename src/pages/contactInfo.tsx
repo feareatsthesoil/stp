4fec69b8150fc47e1e979f27810f05d94d3af1b5
Jonathan <jonathan@jonathans-mbp.attlocal.net>
2023-04-04 17:00:05 -0700

    Locked contact info

--- src/pages/contactInfo.tsx
+++ src/pages/contactInfo.tsx
@@ -7,11 +7,29 @@ import Nav from "../Components/Nav/Nav"
 import DirectoryForm from "../Components/Directory/DirectoryForm"
 import { UserContext } from "../Components/UserContext"
 import { useContext } from "react"
+import { useRouter } from "next/router"
+import { SignIn, useUser } from "@clerk/nextjs"
 
 export default function ContactInfo() {
   const {profile, initialized} = useContext(UserContext)
+  const router= useRouter()
+  const { isSignedIn } = useUser();
   if(!initialized)
     return null
+   
+    if (!isSignedIn)
+      return (
+        <div className="body">
+          <Header />
+          <Nav />
+          <div className="subBody">
+  
+            <div className="box sans">
+              <SignIn afterSignInUrl={router.asPath} />
+            </div>
+          </div>
+        </div>
+      )
   return (
     <>
       <div className="body">
