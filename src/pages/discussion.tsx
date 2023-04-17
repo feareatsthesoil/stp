0be6e732afba009b55af1a1c7040ad901283f8de
“Jonathan” <jvardy@risd.edu>
2023-04-17 02:59:28 -0700

    Updated discussion buttons

--- src/pages/discussion.tsx
+++ src/pages/discussion.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import index from "../styles/Discussion.module.css"
 import DefaultLayout from "../Components/Layouts/DefaultLayout"
+import Button from "@mui/material/Button"
 
 const Discussion = () => {
     return (
@@ -10,9 +11,9 @@ const Discussion = () => {
                 <p>
                     The Serving the People Discord is where our community gathers to discuss and chat. It features a diverse selection of channels, including art, film, video, tech, and more.
                 </p>
-                <button className={index.button}>
+                <Button className={index.button} target="_blank" variant="contained" href="https://discord.com/invite/nhqyng5wQ9">
                     Join the Conversation
-                </button>
+                </Button>
                 <h1>Urbit</h1>
                 <p>
                     Urbit is a new OS and peer-to-peer network that is designed to be simple, built to last forever, and 100% owned by its users. Urbit's Landscape, a collection of native apps for communicating and collaborating on the network, presents the possibility of a new way forward.
@@ -20,9 +21,9 @@ const Discussion = () => {
                 <p>
                     Serving the People has partnered with the Tlon Corporation to offer free hosting (usually $20/month) to members of Serving the People. Join us and a cohort of communities making the move to Urbit's Landscape.
                 </p>
-                <button className={index.button}>
+                <Button className={index.button} target="_blank" variant="contained" href="https://urbit.org/">
                     Learn More
-                </button>
+                </Button>
             </div>
         </DefaultLayout>
     )
