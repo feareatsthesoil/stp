b42d738ea3254a6028ef9372ccec0e6e946b270a
“Jonathan” <jvardy@risd.edu>
2023-05-08 17:11:43 -0700

    fix: blog stats width

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -39,10 +39,11 @@ export default function Blog() {
                                 <div className={css.stats}>
                                     <ul>
                                         <li>
-                                            {post.post_date}
+                                            <p> {post.post_date}</p>
                                         </li>
                                         <li>
-                                            <svg viewBox="0 -3.1 30 24" height={13} fill="transparent" stroke="rgb(112, 112, 112)" stroke-linecap="round" stroke-linejoin="round"><path d="M20.42 4.58a5.4 5.4 0 00-7.65 0l-.77.78-.77-.78a5.4 5.4 0 00-7.65 0C1.46 6.7 1.33 10.28 4 13l8 8 8-8c2.67-2.72 2.54-6.3.42-8.42z"></path></svg> {post.reactions['❤']}
+                                            <svg viewBox="0 -4 30 24" height={13} fill="transparent" stroke="rgb(112, 112, 112)" stroke-linecap="round" stroke-linejoin="round"><path d="M20.42 4.58a5.4 5.4 0 00-7.65 0l-.77.78-.77-.78a5.4 5.4 0 00-7.65 0C1.46 6.7 1.33 10.28 4 13l8 8 8-8c2.67-2.72 2.54-6.3.42-8.42z"></path></svg> <div className={css.reaction}><p>{post.reactions['❤']}
+                                            </p></div>
                                         </li>
                                     </ul>
                                 </div>
