595501e95e23da81e88e16c52bf6b099e35f7c41
“Jonathan” <jvardy@risd.edu>
2023-05-31 14:11:01 -0700

    style: center loader

--- src/Components/Loader/index.tsx
+++ src/Components/Loader/index.tsx
@@ -1,46 +1,10 @@
-import { useEffect, useState } from "react";
-import { CSSProperties } from "styled-components";
-
 import css from "./Loader.module.css";
-import { useSideNav } from "../Nav/NavContext";
 
-interface LoaderProps {
-  initialWindowWidth?: number;
-}
+export default function Loader() {
 
-export default function Loader({ initialWindowWidth }: LoaderProps) {
-  const { sideNavVisible } = useSideNav();
-  const [windowWidth, setWindowWidth] = useState<number | null>(
-    initialWindowWidth || null
-  );
-
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setWindowWidth(window.innerWidth);
-
-      const handleResize = () => {
-        setWindowWidth(window.innerWidth);
-      };
-
-      window.addEventListener("resize", handleResize);
-
-      return () => {
-        window.removeEventListener("resize", handleResize);
-      };
-    }
-  }, []);
-
-  const loadingStyle: CSSProperties = {
-    overflowX: "hidden",
-    width: sideNavVisible
-      ? "calc(100vw - 130px)"
-      : windowWidth && windowWidth <= 575
-        ? "100vw"
-        : "",
-  };
   return (
     <div className={css.wrapper}>
-      <h2 className={css.loading} style={loadingStyle}>
+      <h2 className={css.loading}>
         Loading
       </h2>
     </div>
