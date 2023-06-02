f772c520840667a1627f584ece3f6aabbfea7c13
“Jonathan” <jvardy@risd.edu>
2023-06-02 13:23:13 -0700

    refactor(api/calendar/next): optimize google calendar event fetching function

--- src/pages/api/calendar/next.tsx
+++ src/pages/api/calendar/next.tsx
@@ -1,38 +1,37 @@
-import { NextApiResponse } from "next"
-// import { PrismaClient } from "@prisma/client"
-import { withAuth } from "@clerk/nextjs/api"
-import { google } from "googleapis"
+import { NextApiResponse } from "next";
+import { withAuth } from "@clerk/nextjs/api";
+import { google } from "googleapis";
 
-const EventUpdate = (async (req: any, res: NextApiResponse) => {
-    const { id } = req.query
-    const { userId } = req.auth
-    const { id: _id, ...data } = req.body
-    const { body } = req
-
-    const target = ["https://www.googleapis.com/auth/calendar"]
+const EventUpdate = async (req: any, res: NextApiResponse) => {
     const jwt = new google.auth.JWT(
         process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
         undefined,
         (process.env.GOOGLE_CLOUD_PRIVATE_KEY || "").replace(/\\n/g, "\n"),
-        target
-    )
-    const calendar = google.calendar({ auth: jwt, version: "v3" })
-    const calendarId = process.env.GOOGLE_CALENDAR_ID
+        ["https://www.googleapis.com/auth/calendar"]
+    );
 
-    const response = await calendar.events.list({
-        calendarId: calendarId,
-        timeMin: (new Date()).toISOString(),
-        singleEvents: true,
-        orderBy: 'startTime',
-    });
+    const calendar = google.calendar({ auth: jwt, version: "v3" });
+    const calendarId = process.env.GOOGLE_CALENDAR_ID;
 
-    const events = response.data.items;
-    if (events && events.length) {
-        const nextEvent = events[0];
-        return res.status(200).json(nextEvent);
-    } else {
-        return res.status(404).json({ message: "No upcoming events found" });
+    try {
+        const response = await calendar.events.list({
+            calendarId: calendarId,
+            timeMin: (new Date()).toISOString(),
+            singleEvents: true,
+            orderBy: "startTime",
+        });
+
+        const events = response.data.items;
+        if (events && events.length) {
+            const nextEvent = events[0];
+            return res.status(200).json(nextEvent);
+        } else {
+            return res.status(404).json({ message: "No upcoming events found" });
+        }
+    } catch (error) {
+        console.error("Google API error", error);
+        return res.status(500).json({ message: "Error fetching events" });
     }
-})
+};
 
-export default withAuth(EventUpdate)
+export default withAuth(EventUpdate);
