81d6d4429ecbad4b6935f230a86d36adf2c5e417
“Jonathan” <jvardy@risd.edu>
2023-07-23 12:35:26 -0700

    chore: revert openai

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -1,12 +1,15 @@
-import axios from 'axios'
-export function moderate(text: string){
-    return axios.post("https://api.openai.com/v1/moderations", {input: text}, {headers: {"Authorization": `Bearer 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`}} ).then(response=>{
-       
-        return response.data.results[0]
-       
-    }).catch((err=>{
-        console.error(err.response?.data)
-    }))
-
-    
+import axios from "axios";
+export function moderate(text: string) {
+  return axios
+    .post(
+      "https://api.openai.com/v1/moderations",
+      { input: text },
+      { headers: { Authorization: `Bearer ${process.env.OPEN_API_KEY}` } }
+    )
+    .then((response) => {
+      return response.data.results[0];
+    })
+    .catch((err) => {
+      console.error(err.response?.data);
+    });
 }
