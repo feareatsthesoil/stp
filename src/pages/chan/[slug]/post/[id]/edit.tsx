359aeeb1dfe02268ac81adbe1b1123e4b455f0ef
“Jonathan” <jvardy@risd.edu>
2023-08-06 12:31:37 -0700

    fix: post edit page

--- src/pages/chan/[slug]/post/[id]/edit.tsx
+++ src/pages/chan/[slug]/post/[id]/edit.tsx
@@ -7,41 +7,30 @@ import PostDeleteButton from "../../../../../Components/Posts/PostDeleteButton";
 import PostForm from "../../../../../Components/Posts/PostForm";
 import { getBoard, getPost } from "../../../../../utils/services";
 import { PostResponse } from "../../../../../types";
-import { useAuth } from "@clerk/nextjs"; // Import useAuth
-import { UserContext } from "../../../../../Components/UserContext"; // Assuming the path is similar
+import { useAuth } from "@clerk/nextjs";
+import { UserContext } from "../../../../../Components/UserContext";
 
 export default function PostEditPage() {
-  const { userId } = useAuth(); // Get the current user's ID
-  const userData = useContext(UserContext); // Use context to get user data
+  const { userId } = useAuth();
+  const userData = useContext(UserContext);
   const router = useRouter();
   const { id, slug } = router.query;
-  const [post, setPost] = useState<PostResponse>();
-  const [board, setBoard] = useState<Boards>();
+  const [post, setPost] = useState<PostResponse | null>(null);
+  const [board, setBoard] = useState<Boards | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const [showCookieBanner, setShowCookieBanner] = useState(false);
 
   useEffect(() => {
-    if (id) {
+    if (id && slug) {
+      setIsLoading(true);
       getPost(slug as string, Number(id) as number).then((data) => {
         setPost(data);
+        setIsLoading(false);
       });
-    }
-  }, [id]);
 
-  useEffect(() => {
-    if (slug) {
       getBoard(slug as string).then((data) => setBoard(data));
     }
-  }, [slug]);
-
-  useEffect(() => {
-    if (id) {
-      getPost(slug as string, Number(id) as number).then((data) => {
-        setPost(data);
-        setIsLoading(false); // Set loading to false after data is fetched
-      });
-    }
-  }, [id]);
+  }, [id, slug]);
 
   const handleBackClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
@@ -63,11 +52,78 @@ export default function PostEditPage() {
       </DefaultLayout>
     );
   }
-  
-  if (!post || post.userId !== userId)
+
+  if (!post) {
     return (
       <DefaultLayout>
-        <div className="h-full place-content-center">
+        <p>Error: Post data not loaded.</p>
+      </DefaultLayout>
+    );
+  }
+
+  console.log(post.userId, userId);
+
+  return (
+    <div className="bg-[#F4F4FE]">
+      <DefaultLayout>
+        {post.isAuthor ? (
+          <>
+            <p className="my-2">
+              <Link
+                className="font-black hover:underline"
+                href="#"
+                onClick={handleBackClick}
+              >
+                <button
+                  type="submit"
+                  color="rgb(239, 240, 240)"
+                  className="w-15 h-7 self-center rounded-md bg-[#eff0f0] px-2 font-sans text-sm font-normal text-[#4a4d50] hover:bg-[#e5e6e6] sm:h-5 sm:text-xs"
+                >
+                  Back
+                </button>
+              </Link>
+            </p>
+            {showCookieBanner && (
+              <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
+                <div className="w-min-full rounded-xl bg-white p-6 shadow-lg ring-1 ring-gray-900/10">
+                  <p className="text-center font-sans text-lg font-bold leading-6 text-gray-900">
+                    Are you sure you want to leave?
+                  </p>
+                  <p className="text-center font-sans text-sm font-bold leading-6 text-gray-500">
+                    Remember to save changes.
+                  </p>
+                  <div className="flex place-content-center gap-x-2">
+                    <button
+                      type="button"
+                      className="w-15 mt-2 h-8 rounded-md bg-red-200 px-2 font-sans text-sm font-normal text-red-500 hover:bg-red-300 hover:text-red-600"
+                      onClick={handleYes}
+                    >
+                      Leave
+                    </button>
+                    <button
+                      type="button"
+                      className="w-15 float-right mt-2 h-8 rounded-md bg-[#eff0f0] px-2 font-sans text-sm font-normal text-[#4a4d50] hover:bg-[#e5e6e6]"
+                      onClick={handleNo}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )}
+            <div className="flex flex-col">
+              <PostForm post={post} slug={slug as string} />
+              <div className="mb-2 w-full max-w-[500px] self-center">
+                <PostDeleteButton
+                  postId={post.id}
+                  onComplete={() => router.push("/chan")}
+                >
+                  Delete Post
+                </PostDeleteButton>
+              </div>
+            </div>
+          </>
+        ) : (
           <div className="mt-4 border-l-4 border-yellow-400 bg-yellow-50 p-4">
             <div className="flex">
               <div className="flex-shrink-0">
@@ -93,66 +149,7 @@ export default function PostEditPage() {
               </div>
             </div>
           </div>
-        </div>
-      </DefaultLayout>
-    );
-  return (
-    <div className="bg-[#F4F4FE]">
-      <DefaultLayout>
-        <p className="my-2">
-          <Link
-            className="font-black hover:underline"
-            href="#"
-            onClick={handleBackClick}
-          >
-            <button
-              type="submit"
-              color="rgb(239, 240, 240)"
-              className="w-15 h-7 self-center rounded-md bg-[#eff0f0] px-2 font-sans text-sm font-normal text-[#4a4d50] hover:bg-[#e5e6e6] sm:h-5 sm:text-xs"
-            >
-              Back
-            </button>
-          </Link>
-        </p>
-        {showCookieBanner && (
-          <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="w-min-full rounded-xl bg-white p-6 shadow-lg ring-1 ring-gray-900/10">
-              <p className="text-center font-sans text-lg font-bold leading-6 text-gray-900">
-                Are you sure you want to leave?
-              </p>
-              <p className="text-center font-sans text-sm font-bold leading-6 text-gray-500">
-                Remember to save changes.
-              </p>
-              <div className="flex place-content-center gap-x-2">
-                <button
-                  type="button"
-                  className="w-15 mt-2 h-8 rounded-md bg-red-200 px-2 font-sans text-sm font-normal text-red-500 hover:bg-red-300 hover:text-red-600"
-                  onClick={handleYes}
-                >
-                  Leave
-                </button>
-                <button
-                  type="button"
-                  className="w-15 float-right mt-2 h-8 rounded-md bg-[#eff0f0] px-2 font-sans text-sm font-normal text-[#4a4d50] hover:bg-[#e5e6e6]"
-                  onClick={handleNo}
-                >
-                  Cancel
-                </button>
-              </div>
-            </div>
-          </div>
         )}
-        <div className="flex flex-col">
-          <PostForm post={post} slug={slug as string} />
-          <div className="mb-2 w-full max-w-[500px] self-center">
-            <PostDeleteButton
-              postId={post.id}
-              onComplete={() => router.push("/chan")}
-            >
-              Delete Post
-            </PostDeleteButton>
-          </div>
-        </div>
       </DefaultLayout>
     </div>
   );
