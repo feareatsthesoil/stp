92b7202c9a1b2281e8b2b02366130407fcd8fb36
“Jonathan” <jvardy@risd.edu>
2023-08-10 19:44:46 -0700

    style: post loader

--- src/Components/Posts/PostLoadingState.tsx
+++ src/Components/Posts/PostLoadingState.tsx
@@ -9,31 +9,33 @@ export const LoadingState: React.FC<LoadingStateProps> = ({}) => {
     <>
       <div className="overflow-hidden">
         <DefaultLayout>
-          <div
-            className="ml-8 mt-3 flex h-max w-screen flex-col items-start"
-            style={{ maxWidth: "1000px" }}
-          >
+          <div className="items-left mt-4 flex w-[100%] flex-col">
+            <button className="my-1 h-6 w-[80%] animate-pulse cursor-default rounded-md bg-[#fff] px-2 font-sans text-sm font-normal text-[#1d205e] hover:opacity-80 sm:text-xs" />
             <button className="my-1 h-6 w-[50%] animate-pulse cursor-default rounded-md bg-[#fff] px-2 font-sans text-sm font-normal text-[#1d205e] hover:opacity-80 sm:text-xs" />
-            <button className="my-1 h-6 w-[20%] animate-pulse cursor-default rounded-md bg-[#fff] px-2 font-sans text-sm font-normal text-[#1d205e] hover:opacity-80 sm:text-xs" />
-            <button className="my-1 h-6 w-[30%] animate-pulse cursor-default rounded-md bg-[#fff] px-2 font-sans text-sm font-normal text-[#1d205e] hover:opacity-80 sm:text-xs" />
-            <div className="relative w-[50%]">
+            <button className="my-1 h-6 w-[60%] animate-pulse cursor-default rounded-md bg-[#fff] px-2 font-sans text-sm font-normal text-[#1d205e] hover:opacity-80 sm:text-xs" />
+            <div className="relative w-[80%]">
               <button
                 className="my-1 w-[100%] animate-pulse cursor-default rounded-md bg-[#fff] px-2 pt-[100%] font-sans text-sm font-normal text-[#1d205e] hover:opacity-80 sm:text-xs"
-                style={{ maxWidth: "500px", maxHeight: "500px" }}
+                style={{
+                  maxWidth: "500px",
+                  maxHeight: "500px",
+                  minWidth: "250px",
+                  minHeight: "250px",
+                }}
               />
             </div>
             <ul
               role="list"
-              className="mb-8 mt-2 w-fit space-y-2"
+              className="mb-4 mt-2 w-fit space-y-2"
               style={{ width: "calc(100vw - 2rem)", maxWidth: "500px" }}
             >
-              {Array.from({ length: 30 }, (_, index) => {
+              {Array.from({ length: 5 }, (_, index) => {
                 const buttonWidth = buttonWidths[index % 3];
                 return (
                   <li className="relative flex gap-x-4" key={index}>
                     <div
                       className={`absolute left-0 top-0 flex w-6 animate-pulse justify-center ${
-                        index === -1 ? "h-2" : "-bottom-2"
+                        index === -1 ? "h-4" : index === 4 ? "h-4" : "-bottom-4"
                       }`}
                     >
                       <div className="w-px animate-pulse bg-gray-200" />
