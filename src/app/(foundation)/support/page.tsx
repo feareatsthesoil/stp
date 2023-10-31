72f2882d643becad92fc7f74f8884268811dba6a
feareatsthesoil <105951475+feareatsthesoil@users.noreply.github.com>
2023-10-30 20:27:42 -0400

    Update page.tsx

--- src/app/(foundation)/support/page.tsx
+++ src/app/(foundation)/support/page.tsx
@@ -14,14 +14,6 @@ const Membership = () => {
       <div>
         <h1>We need your support!</h1>
         <p>
-          {`Serving the People is a 501(c)(3) non-profit organization that assists
-          artists and creators in making meaningful connections both online and
-          in person. Established in 2017, STP has launched a number of
-          initiatives and developed a platform for connecting creators with
-          audiences, as well as finding opportunities for collaboration and
-          support.`}
-        </p>
-        <p>
           {`Your contribution directly supports Serving the People's public
           programs. By joining our community, you can enjoy priority access to
           events and support artists globally.`}
